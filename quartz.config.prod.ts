9ae3edd48ec49e1c8ab16c86ee524182fa325e34
wukddang <dnr8874@naver.com>
2025-08-25 22:36:37 +0900

    baseUrl에 https:// 프로토콜 추가

--- quartz.config.prod.ts
+++ quartz.config.prod.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ko-KR",
-    baseUrl: "wukdddang.github.io",
+    baseUrl: "https://wukdddang.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
